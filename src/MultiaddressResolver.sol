82efe9f70332d976d501a828960d35f27b873b4f
katzman <steve.katzman@coinbase.com>
2024-10-08 15:20:57 -0700

    lint

--- src/MultiaddressResolver.sol
+++ src/MultiaddressResolver.sol
@@ -11,45 +11,47 @@ abstract contract MultiaddressResolver is IMultiaddressResolver, ResolverBase {
 
     uint256 private constant COIN_TYPE_ETH = 60;
 
-    mapping(uint64 version => mapping(bytes32 node => mapping(uint256 cointype => EnumerableSetLib.AddressSet addressSet))) versionable_addressSets;
+    mapping(
+        uint64 version => mapping(bytes32 node => mapping(uint256 cointype => EnumerableSetLib.AddressSet addressSet))
+    ) versionable_addressSets;
 
     function addAddrs(bytes32 node, address[] calldata addresses) external virtual authorised(node) {
         addAddrs(node, addresses, COIN_TYPE_ETH);
     }
 
-    function addAddrs(bytes32 node, address[] calldata addresses, uint256 cointype) public virtual  authorised(node) {
+    function addAddrs(bytes32 node, address[] calldata addresses, uint256 cointype) public virtual authorised(node) {
         uint64 version = recordVersions[node];
-        for(uint256 i; i < addresses.length; i++){
-           versionable_addressSets[version][node][cointype].add(addresses[i]);
+        for (uint256 i; i < addresses.length; i++) {
+            versionable_addressSets[version][node][cointype].add(addresses[i]);
         }
         emit AddressesAdded(node, cointype, addresses);
     }
 
-    function removeAddrs(bytes32 node, address[] calldata addresses) external virtual  authorised(node) {
+    function removeAddrs(bytes32 node, address[] calldata addresses) external virtual authorised(node) {
         removeAddrs(node, addresses, COIN_TYPE_ETH);
     }
 
-    function removeAddrs(bytes32 node, address[] calldata addresses, uint256 cointype) public virtual authorised(node) {
+    function removeAddrs(bytes32 node, address[] calldata addresses, uint256 cointype)
+        public
+        virtual
+        authorised(node)
+    {
         uint64 version = recordVersions[node];
-        for(uint256 i; i < addresses.length; i++){
-           versionable_addressSets[version][node][cointype].remove(addresses[i]);
+        for (uint256 i; i < addresses.length; i++) {
+            versionable_addressSets[version][node][cointype].remove(addresses[i]);
         }
         emit AddressesRemoved(node, cointype, addresses);
     }
 
-    function addrs(bytes32 node, uint256 cointype) external virtual view returns (address[] memory) {
+    function addrs(bytes32 node, uint256 cointype) external view virtual returns (address[] memory) {
         return versionable_addressSets[recordVersions[node]][node][cointype].values();
     }
 
-    function addrs(bytes32 node) external virtual view returns (address[] memory) {
+    function addrs(bytes32 node) external view virtual returns (address[] memory) {
         return versionable_addressSets[recordVersions[node]][node][COIN_TYPE_ETH].values();
     }
 
-    function supportsInterface(
-        bytes4 interfaceID
-    ) public view virtual override returns (bool) {
-        return
-            interfaceID == type(IMultiaddressResolver).interfaceId ||
-            super.supportsInterface(interfaceID);
+    function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
+        return interfaceID == type(IMultiaddressResolver).interfaceId || super.supportsInterface(interfaceID);
     }
 }
