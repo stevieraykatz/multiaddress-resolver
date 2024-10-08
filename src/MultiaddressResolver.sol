38859bbbb54eb446b1e1faf17541d9f422f6245f
katzman <steve.katzman@coinbase.com>
2024-10-08 14:57:25 -0700

    start impl

--- src/MultiaddressResolver.sol
+++ src/MultiaddressResolver.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.23;
 
 import {IMultiaddressResolver} from "./IMultiaddressResolver.sol";
+import {ResolverBase} from "ens-contracts/resolvers/ResolverBase.sol";
 
-contract MultiaddressResolver is IMultiaddressResolver {
+contract MultiaddressResolver is IMultiaddressResolver, ResolverBase {
     
+
+    uint256 private constant COIN_TYPE_ETH = 60;
+
+    mapping()
 
     function addrs(bytes32 node, uint256 cointype) external view returns (address[] memory) {
         
@@ -14,4 +19,11 @@ contract MultiaddressResolver is IMultiaddressResolver {
 
     }
 
+    function supportsInterface(
+        bytes4 interfaceID
+    ) public view virtual override returns (bool) {
+        return
+            interfaceID == type(IMultiaddressResolver).interfaceId ||
+            super.supportsInterface(interfaceID);
+    }
 }
